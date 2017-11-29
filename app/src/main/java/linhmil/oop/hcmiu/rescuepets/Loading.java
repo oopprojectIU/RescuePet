47ba185c0967a0e1cb4fe5ac3af282e8976cce29
suhymin97 <mity.2a@gmail.com>
2017-11-29 21:46:58 +0700

    initialize project AS

---
+++ app/src/main/java/linhmil/oop/hcmiu/rescuepets/Loading.java
@@ -0,0 +1,13 @@
+package linhmil.oop.hcmiu.rescuepets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Loading extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
